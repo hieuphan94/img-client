bc6e8e86624fa82b44793a9cc2a1a0ddb5358f36
hieuphan94 <phantronghieu1@gmail.com>
2021-12-04 21:47:07 +0700

    Fix API 2

--- src/data/api/api.js
+++ src/data/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://159.223.92.252/api",
+  baseURL: "http://api.imagetravel.vn/api",
 });
--- src/data/api/tours.js
+++ src/data/api/tours.js
@@ -1,4 +1,4 @@
-import API from "./API";
+import API from "./api";
 
 export const TourAPI = {
   getTour: async (url) => {
